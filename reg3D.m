d2eb36661e93ff7f4bf31e94e6c2c5b40e2492e9
sakumashirayuki <2909549511@qq.com>
2020-03-06 17:45:52 +0800

    change iter ending condition, RMS, add figure showing register coordinates

--- reg3D.m
+++ reg3D.m
@@ -1,44 +1,56 @@
-function [s,R,T,e,it]=reg3D(file1,file2)
+function [s,R,T,e,it]=reg3D(data1,data2,varargin)
+%变换data1去接近data2
+%改为变换data2去接近data1
+%data1的点少于data2
 %s 数据单位量纲缩放值
 %R 旋转矩阵
 %T 平移矩阵
-%e 误差？
+%e 误差
 %it 迭代次数
-%file1和file2为点云输入文件，点数可以不一样
+%新输入：2*1d cell,cell{1}为点个数，cell{2}储存点的坐标，大小为n维*点数
+%迭代次数：默认10次
+%收敛误差
 
-data1 = ascread(file1);%40097points
-data2 = ascread(file2);%40256points
+%输入参数处理，data1和data2
+p=inputParser;
+p.addRequired('data1',@(x)validateattributes(x,{'cell'},{'nonempty'}));
+p.addRequired('data2',@(x)validateattributes(x,{'cell'},{'nonempty'}));
+%迭代次数和收敛误差为可选输入
+p.addOptional('iter',10,@(x)validateattributes(x,{'int8'},{'nonzero'}));
+p.addOptional('error',0.001,@(x)validateattributes(x,{'double'},{'nonzero'}));
+p.parse(data1,data2,varargin{:});
 
-X = data1{2};
-Y = data2{2};
+X = p.Results.data1{2};
+Y = p.Results.data2{2};
 
-initD = init(data1,data2);
+initD = init(data2,data1);
 R0 = initD{1};
 T0 = initD{2};
 s0 = initD{3};
 I = initD{4};
-o = 0.001;
 
 %进行delaunay三角剖分
 Yo = delaunayn(Y');
 
 tic;
 %迭代求解部分
-c0 = Solvecircle(s0,R0,T0,I,X,Y,Yo);%??????????????
+c0 = Solvecircle(s0,R0,T0,I,X,Y,Yo);
 Rn = c0{1};
 Tn = c0{2};
 sn = c0{3};
-etemp = c0{5};%迭代次数
-q=1;
-flag = 2;
-while (q>o)
+etemp = c0{5};%当前误差
+flag = 2;%迭代次数
+while (etemp>p.Results.error)
     c = Solvecircle(sn,Rn,Tn,I,X,Y,Yo);
     Rn = c{1};
     Tn = c{2};
     sn = c{3};
-    en = c{5};
-    q = 1 - en/etemp;
+    en = c{5};%变换后的误差
+    % q = 1 - en/etemp;
     etemp = en;
+    if flag>=p.Results.iter
+        break;%大于迭代次数则退出
+    end
     flag = flag + 1; 
 end 
 s = sn;
--- test.m
+++ test.m
@@ -1,1 +1,7 @@
-[s,R,T,e,it] = reg3D('bun045.asc','bun000.asc')
+% ÓÃÓÚreg3DµÄ²âÊÔ
+file1 = 'bun045.asc';
+file2 = 'bun000.asc';
+data1 = ascread(file1);%40097points
+data2 = ascread(file2);%40256points
+%[s,R,T,e,it] = reg3D('bun045.asc','bun000.asc')
+[s,R,T,e,it] = reg3D(data1,data2)
